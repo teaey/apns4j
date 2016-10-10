b6f81e26d714f1b8e4492d4d25330261e086da0d
teaey <masfay@me.com>
2016-10-10 22:19:47 +0800

    1.1.1

--- src/test/java/cn/teaey/apns4j/TestConts.java
+++ src/test/java/cn/teaey/apns4j/TestConts.java
@@ -5,7 +5,7 @@ package cn.teaey.apns4j;
  * @since 1.0.3
  */
 public interface TestConts {
-    String deviceToken = "81730841daf47269dfcf2ce30af1daa71bac889e08e08ecfcd13266d11df00fb";
+    String deviceToken = "32252032dc502ded559698b085227799af59abd4d8343f4027012f156f2edc23";
     String deviceToken2 = "a9d04e8d98adb00fa1a62992901b7990f03ac9ba200635c85cbcfdeb464d85ee";
 
     String keyStorePath = "/Users/teaey/Documents/apns4j/aps_development.p12";
